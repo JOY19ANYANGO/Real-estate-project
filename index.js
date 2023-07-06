9a47243b900638527128f4126037f3068cb5fa4b
JOY19ANYANGO <joyanyango402@gmail.com>
2023-07-06 14:10:47 +0300

    fetched rental house details

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const home = document.getElementById("homebtn");
-  home.addEventListener("click", fetchAndRenderMovieDetails);
+  const homeBtn = document.getElementById("homebtn");
+  const rentalBtn = document.getElementById("rentalbtn");
 
-  function fetchAndRenderMovieDetails() {
-    fetch("http://localhost:3000/Houses")
-      .then((res) => res.json())
-      .then((data) => {
-        const details = document.getElementById("homes");
-        
-        // Clear previous details
-        details.innerHTML = '';
-  
-        // Loop through houses and render details
-        data.forEach((house) => {
-          const houseElement = document.createElement('div');
-          houseElement.innerHTML = `
-            <p>Title: ${house.title}</p>
-            <p>Location: ${house.location}</p>
-            <p>Features: ${house.features}</p>
-            <p>Amenities: ${house.amenities}</p>
-            <img src="${house.image}" id="fetchedimages">
-          `;
-          details.appendChild(houseElement);
-        });
+  homeBtn.addEventListener("click", fetchAndRenderHomeDetails);
+  rentalBtn.addEventListener("click", fetchAndRenderRentalsDetails);
+});
+
+function fetchAndRenderHomeDetails() {
+  const ids = [1, 2, 3]; // IDs of the houses you want to fetch
+
+  const fetchRequests = ids.map((id) => {
+    return fetch(`http://localhost:3000/Houses/${id}`)
+      .then((res) => res.json());
+  });
+
+  Promise.all(fetchRequests)
+    .then((houses) => {
+      const details = document.getElementById("homes");
+
+      // Clear previous details
+      details.innerHTML = '';
+
+      // Loop through houses and render details
+      houses.forEach((house) => {
+        const houseElement = document.createElement('div');
+        houseElement.innerHTML = `
+          <p>Title: ${house.title}</p>
+          <p>Location: ${house.location}</p>
+          <p>Features: ${house.features}</p>
+          <p>Amenities: ${house.amenities}</p>
+          <img src="${house.image}" id="fetchedimages">
+        `;
+        details.appendChild(houseElement);
       });
-  }
-})  
+    })
+    .catch((error) => {
+      console.error('Error fetching house details:', error);
+    });
+}
+
+function fetchAndRenderRentalsDetails() {
+  const ids = [4, 5, 6]; // IDs of the houses you want to fetch
+
+  const fetchRequests = ids.map((id) => {
+    return fetch(`http://localhost:3000/Houses/${id}`)
+      .then((res) => res.json());
+  });
+
+  Promise.all(fetchRequests)
+    .then((houses) => {
+      const info = document.getElementById("rentals");
+
+      // Clear previous details
+      info.innerHTML = '';
+
+      // Loop through houses and render details
+      houses.forEach((house) => {
+        const houseElement = document.createElement('div');
+        houseElement.innerHTML = `
+          <p>Title: ${house.title}</p>
+          <p>Location: ${house.location}</p>
+          <p>Features: ${house.features}</p>
+          <p>Amenities: ${house.amenities}</p>
+          <p>Price: ${house.price}</p>
+          <img src="${house.image}" id="fetchedimages">
+        `;
+        info.appendChild(houseElement);
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching house details:', error);
+    });
+}
